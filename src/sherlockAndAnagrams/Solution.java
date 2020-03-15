65920c665860ba627341f9344402670120523440
ali <ali@AliComputer>
2020-03-15 21:06:35 +0100

    FirstCommit

---
+++ src/sherlockAndAnagrams/Solution.java
@@ -0,0 +1,49 @@
+package sherlockAndAnagrams;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the sherlockAndAnagrams function below.
+    static int sherlockAndAnagrams(String s) {
+        int counter = 0;
+        for (int j = 0; j <  s.length()-1; j++) {
+            for (int i = j+1; i < s.length(); i++) {
+                String sub = s.substring(j, i);
+                sub = new StringBuilder(sub).reverse().toString();
+                if (s.contains(sub)) {
+                    counter++;
+                }
+            }
+        }
+        
+        return counter;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s = scanner.nextLine();
+
+            int result = sherlockAndAnagrams(s);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
