f1f74cf376de88deed54d7d0ba2c24564310b0f1
ali <ali@AliComputer>
2020-03-16 19:59:27 +0100

    solved

--- src/sherlockAndAnagrams/Solution.java
+++ src/sherlockAndAnagrams/Solution.java
@@ -1,4 +1,5 @@
 package sherlockAndAnagrams;
+
 import java.io.*;
 import java.math.*;
 import java.security.*;
@@ -9,41 +10,58 @@ import java.util.regex.*;
 
 public class Solution {
 
-    // Complete the sherlockAndAnagrams function below.
-    static int sherlockAndAnagrams(String s) {
-        int counter = 0;
-        for (int j = 0; j <  s.length()-1; j++) {
-            for (int i = j+1; i < s.length(); i++) {
-                String sub = s.substring(j, i);
-                sub = new StringBuilder(sub).reverse().toString();
-                if (s.contains(sub)) {
-                    counter++;
+	static String getStringKey(String s) {
+		char[] sArray = s.toCharArray();
+		Arrays.sort(sArray);
+		return new String(sArray);
+	}
+	// Complete the sherlockAndAnagrams function below.
+	static int sherlockAndAnagrams(String s) {
+		Map<String, Integer> patterns = new HashMap<>();
+        for (int i = 0 ; i <= s.length(); i++) {
+            for (int j = i; j <= s.length(); j++) {
+            	String subString = getStringKey(s.substring(i,j));
+            	System.out.println(subString);
+            	if (subString.length() > 0) {
+                Integer value = patterns.get(subString);
+                if (value == null) {
+                    patterns.put(subString, 1);
+                } else {
+                    patterns.put(subString, value+1);
+                }
                 }
             }
         }
+        int counter = 0;
         
-        return counter;
-    }
-
-    private static final Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        int q = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        for (int qItr = 0; qItr < q; qItr++) {
-            String s = scanner.nextLine();
-
-            int result = sherlockAndAnagrams(s);
-
-            bufferedWriter.write(String.valueOf(result));
-            bufferedWriter.newLine();
+        for (Map.Entry<String, Integer> ent: patterns.entrySet()) {
+            int count = ent.getValue();
+            if (count > 1) {
+                counter += (count * (count-1)/2);  
+            }
         }
+        return counter;
+	}
 
-        bufferedWriter.close();
+	private static final Scanner scanner = new Scanner(System.in);
 
-        scanner.close();
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+		int q = scanner.nextInt();
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+		for (int qItr = 0; qItr < q; qItr++) {
+			String s = scanner.nextLine();
+
+			int result = sherlockAndAnagrams(s);
+
+			bufferedWriter.write(String.valueOf(result));
+			bufferedWriter.newLine();
+		}
+
+		bufferedWriter.close();
+
+		scanner.close();
+	}
 }
